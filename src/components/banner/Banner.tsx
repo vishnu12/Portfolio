a4348b96bb16787f372f29d719a27b0262a5fb15
Vishnu <vishnu007jr@gmail.com>
2021-08-12 14:18:25 +0530

    design fixes

--- src/components/banner/Banner.tsx
+++ src/components/banner/Banner.tsx
@@ -9,9 +9,9 @@ export const Banner:React.FC = () => {
                 <h1>Hi I'M Vishnu</h1>
                 <h4>A Self Taught Web Developer</h4>
                 <p>
-                This is my personal website where you can see my professional 
-                background and the set of skills i possess to meet your 
-                requirements.Please find more details by scrolling down.
+                This is my personal website where you can see my professional background 
+                and the set of skills i possess to meet your requirements.Please find more
+                 details by scrolling down.
                 </p>
             </div>
         </Container>
